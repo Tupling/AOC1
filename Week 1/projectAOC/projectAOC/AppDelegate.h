a19484f86715b78f3c2a89d3de29fe40bed75e34
Dale Tupling <tupling32@hotmail.com>
2013-02-08 21:50:58 +0430

    Activities requirements completed
    Week 1 activities completed

---
+++ Week 1/projectAOC/projectAOC/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  projectAOC
+//
+//  Created by Dale Tupling on 2/7/13.
+//  Copyright (c) 2013 Dale Tupling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
